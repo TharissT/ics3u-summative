26e8a4e0c6acf01241a3826946edfdcc39b0fcf1
Thariss <thariss.thavakumar@gmail.com>
2024-12-10 13:54:40 -0500

    Added another important component firebase to the project

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyB6ToXoBPrTM6Aaa5uEIoDcxZvaQsPI0zg",
+    authDomain: "summative-82318.firebaseapp.com",
+    projectId: "summative-82318",
+    storageBucket: "summative-82318.firebasestorage.app",
+    messagingSenderId: "901275301519",
+    appId: "1:901275301519:web:5f1d118e404209025ce2fa"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
